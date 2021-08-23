0bf6f26abb5739c30b3389ee136a387c8d4e7b81
Kenneth Wang <yunfeiw@me.com>
2021-08-23 10:30:32 -0700

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+var fullName = 'Kenneth Wang';
+var isCool = true;
+var totalPets = 1;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+var unknow = null;
+console.log('value of unknow:', unknow);
+console.log('typeof unknow:', typeof unknow);
+
+var never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
