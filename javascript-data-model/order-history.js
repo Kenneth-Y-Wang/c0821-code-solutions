5c118402577c4ac370ec417e4e0d3bc65e948b21
Kenneth Wang <yunfeiw@me.com>
2021-08-24 11:53:37 -0700

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,78 @@
+var orderHistory = [
+
+  {
+    orderId: '114-3941689-8772232',
+    orderItems: [
+      {
+        itemName: 'Javascript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        itemPrice: 31.55,
+        productReview: ''
+      }
+    ],
+    orderTotal: 34.00,
+    shipTo: 'JS Masher',
+    orderPlacedDate: '2020-08-04',
+    orderDeliveredDate: '2020-08-08',
+    returnWindowDate: '2020-09-07'
+  },
+
+  {
+    orderId: '113-9984268-1280257',
+    orderItems: [
+      {
+        itemName: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        itemPrice: 41.33,
+        productReview: ''
+      }
+    ],
+    orderTotal: 44.53,
+    shipTo: 'JS Masher',
+    orderPlacedDate: '2020-07-19',
+    orderDeliveredDate: '2020-07-20',
+    returnWindowDate: '2020-08-19'
+  },
+
+  {
+    orderId: '114-2875557-9059409',
+    orderItems: [
+      {
+        itemName: 'Gamecube Controller Adapter',
+        author: '',
+        itemPrice: 15.98,
+        productReview: ''
+      }
+    ],
+    orderTotal: 17.22,
+    shipTo: 'JS Masher',
+    orderPlacedDate: '2020-07-04',
+    orderDeliveredDate: '2020-07-07',
+    returnWindowDate: '2020-08-05'
+  },
+
+  {
+    orderId: '113-2883177-2648248',
+    orderItems: [
+      {
+        itemName: 'Gamecube Controller',
+        author: '',
+        itemPrice: 94.95,
+        productReview: ''
+      },
+      {
+        itemName: 'The Art of Sql',
+        author: '',
+        itemPrice: 33.99,
+        productReview: ''
+      }
+    ],
+    orderTotal: 138.93,
+    shipTo: 'JS Masher',
+    orderPlacedDate: '2020-07-03',
+    orderDeliveredDate: '2020-07-05',
+    returnWindowDate: '2020-08-04'
+  }
+];
+
+console.log('value of orderHistory:', orderHistory);
