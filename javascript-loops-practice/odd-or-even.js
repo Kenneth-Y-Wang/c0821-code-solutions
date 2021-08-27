18d0e5a162bfda4299c8a5ed823bac98e0b0516a
Kenneth Wang <yunfeiw@me.com>
2021-08-26 20:49:58 -0700

    Completed javascript-loops-practice exercise.

--- javascript-loops-practice/odd-or-even.js
+++ javascript-loops-practice/odd-or-even.js
@@ -1,1 +1,12 @@
 /* exported oddOrEven */
+function oddOrEven(numbers) {
+  var result = [];
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      result.push('even');
+    } else if (numbers[i] % 2 === 1) {
+      result.push('odd');
+    }
+  }
+  return result;
+}
