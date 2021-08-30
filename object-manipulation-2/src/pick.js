0fdb065158c22d522419e7cf1c76d31a57236dd5
Kenneth Wang <yunfeiw@me.com>
2021-08-30 15:13:30 -0700

    in progress

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,10 +1,12 @@
 /* exported pick */
 function pick(source, keys) {
+  debugger;
   var result = {};
-  for (var key in source) {
-    for (var i = 0; i < keys.length; i++) {
-      if (key === keys[i]) {
-        result[key] = source[key];
+
+  for (var i = 0; i < keys.length; i++) {
+    for (var key in source) {
+      if (keys[i] === key && source[keys[i]] !== undefined) {
+        result[keys[i]] = source[key];
       }
 
     }
