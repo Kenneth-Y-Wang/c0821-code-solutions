a66aa33c3ff952746a4284ee46dc9086346ebcc1
Kenneth Wang <yunfeiw@me.com>
2021-08-29 21:17:27 -0700

    in progress

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -12,8 +12,17 @@ function chunk(array, size) {
   return final;
 }
 
-/*
-function chunk(array, size) {
+/* create a storage to store each 'size' of array
+create a final storage for outcome
+if the size is bigger than the array length, then return the array itself as final outcome
+if the size is smaller then the array length, then we start to look at each element in given array
+we add each element from array to the storage 'result'
+once the result storage's length reachs to 'size',then we push the array 'result' to the final array, and empty the result array
+keep the process until the rest of elements from given array are added to 'result' array, then push the result to final array
+return the final array
+
+*/
+/* function chunk(array, size) {
   var result = [];
   var final = [];
   if (size > array.length) {
@@ -21,7 +30,7 @@ function chunk(array, size) {
     return final;
   }
 
-  for (var i = 0; i < array.length; i += size) {
+  for (var i = 0; i < array.length; i++) {
     if (result.length === size) {
       final.push(result);
       result = [];
@@ -32,4 +41,5 @@ function chunk(array, size) {
   final.push(result);
   return final;
 }
+
 */
