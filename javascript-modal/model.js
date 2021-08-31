1c6025e973c8822433ec2f6cd0244ea7ec197272
Kenneth Wang <yunfeiw@me.com>
2021-08-31 15:54:20 -0700

    updated JavaScript Codes

--- javascript-modal/model.js
+++ javascript-modal/model.js
@@ -4,19 +4,7 @@ var $no = document.querySelector('.no');
 var $row = document.querySelector('.row');
 var $overlay = document.querySelector('.overlay');
 
-$press.addEventListener('click', function () {
-  model = !model;
-  if (model === false) {
-    $row.className = 'row';
-    $overlay.className = ' overlay';
-
-  } else {
-    $row.className = 'row off';
-    $overlay.className = ' overlay noshow';
-  }
-});
-
-$no.addEventListener('click', function () {
+function callmodal(event) {
   model = !model;
   if (model === false) {
     $row.className = 'row';
@@ -27,4 +15,6 @@ $no.addEventListener('click', function () {
     $overlay.className = ' overlay noshow';
   }
 }
-);
+
+$press.addEventListener('click', callmodal);
+$no.addEventListener('click', callmodal);
