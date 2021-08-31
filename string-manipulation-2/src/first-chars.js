26df2d0150093196614f991db74c582e42a42596
Kenneth Wang <yunfeiw@me.com>
2021-08-30 17:50:35 -0700

    updated adjustments.

--- string-manipulation-2/src/first-chars.js
+++ string-manipulation-2/src/first-chars.js
@@ -2,9 +2,8 @@
 function firstChars(length, string) {
   var result = ''; /* create the storage for result outcome */
   if (length > string.length) { /* if the given length is larger than the string length */
-    for (var j = 0; j < string.length; j++) { /* then every character of the string will be added to the result string */
-      result += string[j];
-    }
+
+    result = string;
     return result; /* return the result string for this condition */
   }
   for (var i = 0; i < length; i++) { /* if the given length is smaller than the string length, then starting from the first spot, we start to check each character of the string,until the given length spot */
