003e319e4d56028a6bba7b01895e27c47a0adb1c
Kenneth Wang <yunfeiw@me.com>
2021-09-02 11:56:39 -0700

    updated the correction

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -4,9 +4,9 @@ var $taskList = document.querySelector('.task-list');
 $taskList.addEventListener('click', function (e) {
   console.log('event.target', event.target);
   console.log('event.target.tagName', event.target.tagName);
-  var $button = document.querySelector('button');
+
   if (e.target && e.target.tagName === 'BUTTON') {
-    var closestTaskItem = $button.closest('.task-list-item');
+    var closestTaskItem = e.target.closest('.task-list-item');
     console.log('closest. task-list item', closestTaskItem);
     closestTaskItem.remove();
   }
