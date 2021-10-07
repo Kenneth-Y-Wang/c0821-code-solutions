1839ba6f411b69b40c3be58bb7b35c2663e3e376
Kenneth Wang <yunfeiw@me.com>
2021-10-07 17:09:22 +0000

    the loop is corrected

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -15,35 +15,31 @@ if (commandWord === 'read') {
 
   fs.writeFile('data.json', updatedNote, 'utf8', err => {
     if (err) throw err;
-    console.log('the save is good');
+    console.log('Your save is successful');
 
   });
 
 } else if (commandWord === 'delete') {
   const deleteIndex = process.argv[3];
-  for (const key in noteBook.notes) {
-    if (key === deleteIndex) {
-      delete noteBook.notes[key];
-    }
-  }
+
+  delete noteBook.notes[deleteIndex];
+
   const updatedNote = JSON.stringify(noteBook, null, 2);
   fs.writeFile('data.json', updatedNote, 'utf8', err => {
     if (err) throw err;
-    console.log('the save is good');
+    console.log('Your save is successful');
 
   });
 } else if (commandWord === 'update') {
   const editIndex = process.argv[3];
   const editContent = process.argv[4];
-  for (const key in noteBook.notes) {
-    if (key === editIndex) {
-      noteBook.notes[key] = editContent;
-    }
-  }
+
+  noteBook.notes[editIndex] = editContent;
+
   const updatedNote = JSON.stringify(noteBook, null, 2);
   fs.writeFile('data.json', updatedNote, 'utf8', err => {
     if (err) throw err;
-    console.log('the save is good');
+    console.log('Your save is successful');
 
   });
 } else {
