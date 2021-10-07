3de5f1ec0718de8442880bd13d58b6171ca8f8ab
Kenneth Wang <yunfeiw@me.com>
2021-10-07 17:22:16 +0000

    Completed level3 node-fs-readfile

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,79 +1,16 @@
 const fs = require('fs');
-const array = [];
-const index = [];
 
-for (var i = 2; i < process.argv.length; i++) {
-  fs.readFile(process.argv[i], 'utf8', (err, data) => {
+function readFile(a) {
+  fs.readFile(process.argv[a], 'utf8', (err, data) => {
     if (err) throw err;
-    const newFile = {};
-    newFile.index = i;
-    newFile.content = data;
-    array.push(newFile);
-    console.log(array);
+    console.log(data);
 
-  });
+    const NextIndex = a + 1;
 
-}
-for (var a = 0; a < array.length; a++) {
-  for (var key in array[a]) {
-    index.push(key);
-  }
-  console.log(index);
-}
-
-index.sort(function (a, b) { return a - b; });
-
-for (var b = 0; b < index.length; b++) {
-  for (var c = 0; c < array.length; c++) {
-    for (const key in array[c]) {
-      if (key === index[b]) {
-        console.log(array[c][key]);
-      }
+    if (NextIndex < process.argv.length) {
+      readFile(NextIndex);
     }
-  }
+  });
 }
-// function readFile(a) {
-//   fs.readFile(process.argv[a], 'utf8', (err, data) => {
-//     if (err) throw err;
-//     console.log(data);
 
-//   });
-
-//   const NextIndex = a + 1;
-
-//   if (NextIndex < process.argv.length) {
-//     readFile(NextIndex);
-//   }
-// }
-
-// readFile(2);
-
-// let printFile = function f(a) {
-//   fs.readFile(process.argv[a], 'utf8', (err, data) => {
-//     if (err) throw err;
-//     console.log(data);
-//   });
-
-//   const NextIndex = a + 1;
-
-//   if (NextIndex < process.argv.length) {
-//     f(NextIndex);
-//   }
-// };
-
-// const rightOrder = printFile;
-// printFile = null;
-// rightOrder(2);
-// for (var i = 2; i < process.argv.length; i++) {
-//   fs.readFile(process.argv[i], 'utf8', (err, data) => {
-//     if (err) throw err;
-//     console.log(data);
-//   })
-//   ;
-// }
-// const fileName = process.argv[2];
-// fs.readFile(fileName, 'utf8', (err, data) => {
-//   if (err) throw err;
-//   console.log(data);
-// })
-//   ;
+readFile(2);
