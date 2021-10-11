5f0f9c05bb8d78f00094bd6d9f429100b5c671c0
Kenneth Wang <yunfeiw@me.com>
2021-10-11 22:37:37 +0000

    in progress

--- express-socket/index.js
+++ express-socket/index.js
@@ -3,10 +3,10 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-// app.get('/', function (req, res) {
-//   res.sendFile(__dirname + '/home.html');
-// });
-app.use(express.static('public'));
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+// app.use(express.static('public'));
 
 server.listen(3000, () => {
   // eslint-disable-next-line no-console
