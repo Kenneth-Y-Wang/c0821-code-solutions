191cf20f11a8ace703ff65dcf7f25dbbca47f6b3
Kenneth Wang <yunfeiw@me.com>
2021-10-12 17:23:16 +0000

    trying saving data and submit by form

--- express-socket/index.js
+++ express-socket/index.js
@@ -3,10 +3,10 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-// app.use(express.static('public'));
+// app.get('/', function (req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+app.use(express.static('public'));
 
 server.listen(3000, () => {
   // eslint-disable-next-line no-console
@@ -16,7 +16,12 @@ server.listen(3000, () => {
 io.on('connection', function (socket) {
   console.log('socket connected:', socket.id);
   io.emit('user connected');
-  socket.on('message', function (msg) {
-    io.emit('message', msg);
+  socket.on('chat', function (msg) {
+    console.log(msg);
+    io.emit('chat', msg);
+  });
+
+  socket.on('typing', function (data) {
+    socket.broadcast.emit('typing', data);
   });
 });
