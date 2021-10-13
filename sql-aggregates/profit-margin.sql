e9bf0720ccfc3c8321a53075d612a387832680b1
Kenneth Wang <yunfeiw@me.com>
2021-10-13 22:06:21 +0000

    in progress on question 6

--- sql-aggregates/profit-margin.sql
+++ sql-aggregates/profit-margin.sql
@@ -1,19 +1,26 @@
 
+-- with "costForm" as (
+--   select "f"."title" as "title",
+--          "f"."filmId" as "filmId",
+--          "f"."replacementCost" as "replacementCost",
+--          "i"."inventoryId"
+--   from  "films" as "f"
+--   join  "inventory" as "i" using("filmId")
+-- )
+
 with "costForm" as (
-  select "f"."title" as "title",
+select "f"."title" as "title",
          "f"."filmId" as "filmId",
-         "f"."replacementCost" as "replacementCost",
-         "i"."inventoryId"
+         sum("f"."replacementCost") as "totalCost"
   from  "films" as "f"
   join  "inventory" as "i" using("filmId")
+  group by "filmId"
 )
-
-
 
 select "f"."title",
        "f"."description",
        sum("p"."amount") as "totalAmountMade",
-       sum("costForm"."replacementCost") as "totalCost"
+       "costForm"."totalCost"
   from "films" as "f"
   join "inventory" using ("filmId")
   join "rentals" using ("inventoryId")
