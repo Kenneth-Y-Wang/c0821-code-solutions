6fafa64c190f4edc07cc920368355575c9bc8365
Kenneth Wang <yunfeiw@me.com>
2021-10-20 15:06:59 -0700

    Working on face reset

---
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+export default class StopWatch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { time: 0, ticking: false };
+    this.play = this.play.bind(this);
+    this.pause = this.pause.bind(this);
+  }
+
+  tick() {
+    this.setState(state => ({
+      time: state.time + 1
+    }));
+  }
+
+  play() {
+    this.timeID = setInterval(
+      () => this.tick(),
+      1000
+    );
+    this.setState(state => ({
+      ticking: !state.ticking
+    }));
+  }
+
+  pause() {
+    clearInterval(this.timeID);
+    this.setState(state => ({
+      ticking: !state.ticking
+    }));
+  }
+
+  render() {
+    const ticking = this.state.ticking;
+    if (!ticking) {
+      return (
+      <div className="clock-display">
+        <div className="clock">
+          <h1>{this.state.time}</h1>
+        </div>
+        <div className="switch" onClick={this.play}><i className="fas fa-play"></i></div>
+      </div>
+      );
+    } else {
+      return (
+      <div className="clock-display">
+        <div className="clock">
+          <h1>{this.state.time}</h1>
+        </div>
+        <div className="switch" onClick={this.pause}><i className="fas fa-pause"></i></div>
+      </div>
+      );
+    }
+  }
+}
