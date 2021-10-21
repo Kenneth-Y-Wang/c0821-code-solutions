cf1005f84aec3d953c52f086de685fef2ae1eb07
Kenneth Wang <yunfeiw@me.com>
2021-10-21 23:48:37 +0000

    in progress

--- react-app-drawer-component/src/drawer.jsx
+++ react-app-drawer-component/src/drawer.jsx
@@ -5,19 +5,24 @@ export default class Drawer extends React.Component {
   constructor(props) {
     super(props);
     this.state = { isOpen: false };
+    this.switch = this.switch.bind(this);
   }
 
   switch() {
-    this.setState(state => ({
-      isOpen: !state.isOpen
-    }));
+    if (event.target.matches('a') === true || event.target.matches('.drawer-holder') === true || event.target.matches('.fas') === true) {
+      this.setState(state => ({
+        isOpen: !state.isOpen
+      }));
+    }
+
   }
 
   render() {
     const isOpen = this.state.isOpen;
-    return (
+    if (!isOpen) {
+      return (
       <div className="drawer-holder">
-        <div className="switch-button"><i className="fas fa-bars"></i></div>
+        <div className="switch-button" onClick={this.switch}><i className="fas fa-bars"></i></div>
         <div className="drawer">
           <h1>Menu</h1>
           <a>About</a>
@@ -26,6 +31,20 @@ export default class Drawer extends React.Component {
           <a>What do You Like</a>
         </div>
       </div>
-    );
+      );
+    } else {
+      return (
+        <div className="drawer-holder holder-on" onClick={this.switch}>
+          <div className="switch-button" ><i className="fas fa-bars"></i></div>
+          <div className="drawer drawer-on">
+            <h1 >Menu</h1>
+            <a >About</a>
+            <a >Find a Band</a>
+            <a >Want to Chat</a>
+            <a >What do You Like</a>
+          </div>
+        </div>
+      );
+    }
   }
 }
