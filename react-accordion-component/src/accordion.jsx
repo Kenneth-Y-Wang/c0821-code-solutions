520b4a3c9e7c864186aa62d37904d5671e09b60d
Kenneth Wang <yunfeiw@me.com>
2021-10-22 18:30:23 +0000

    correction on condition expressions and this.state setting

--- react-accordion-component/src/accordion.jsx
+++ react-accordion-component/src/accordion.jsx
@@ -3,39 +3,27 @@ import React from 'react';
 export default class Accordion extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { clicked: '', isOpen: false };
+    this.state = { clicked: '' };
     this.click = this.click.bind(this);
   }
 
   click() {
 
     if (this.state.clicked === event.target.id) {
-      this.setState({ isOpen: !this.state.isOpen });
+      this.setState({ clicked: '' });
     } else {
-      this.setState({ clicked: event.target.id, isOpen: false });
+      this.setState({ clicked: event.target.id });
     }
 
   }
 
   render() {
     const topics = this.props.topics;
-    const listItems = topics.map(topic => {
-      if (topic.title === this.state.clicked && !this.state.isOpen) {
-        return (
+    const listItems = topics.map(topic =>
           <div key={topic.title} >
             <div className="topic-header " onClick={this.click} id={topic.title}>{topic.title}</div>
-            <div className="topic-content" >{topic.content}</div>
+            <div className={topic.title === this.state.clicked ? 'topic-content' : 'topic-content hidden'} >{topic.content}</div>
           </div>
-        );
-      } else {
-        return (
-          <div key={topic.title} >
-            <div className="topic-header " onClick={this.click} id={topic.title}>{topic.title}</div>
-            <div className="topic-content hidden" >{topic.content}</div>
-          </div>
-        );
-      }
-    }
     );
 
     return (
