61ca7d9d9c2df71478d9f6c6a2f4c2ce7d5b715a
Kenneth Wang <yunfeiw@me.com>
2021-10-22 22:29:23 +0000

    Completed react-carousel-component exercise

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -4,29 +4,93 @@ export default class Carousel extends React.Component {
   constructor(props) {
     super(props);
     this.state = { index: 1 };
+    this.rightClick = this.rightClick.bind(this);
+    this.leftClick = this.leftClick.bind(this);
+    this.dotClick = this.dotClick.bind(this);
+  }
+
+  autoRun() {
+    this.setState(state => ({
+      index: state.index + 1
+    }));
+    if (this.state.index === this.props.pics.length + 1) {
+      this.setState({ index: 1 });
+    }
+
+  }
+
+  rightClick() {
+    this.setState(state => ({
+      index: state.index + 1
+    }));
+    if (this.state.index === this.props.pics.length) {
+      this.setState({ index: 1 });
+    }
+    clearInterval(this.timeID);
+    this.timeID = setInterval(
+      () => this.autoRun(),
+      3000
+    );
+
+  }
+
+  leftClick() {
+    this.setState(state => ({
+      index: state.index - 1
+    }));
+    if (this.state.index === 1) {
+      this.setState({ index: this.props.pics.length });
+    }
+    clearInterval(this.timeID);
+    this.timeID = setInterval(
+      () => this.autoRun(),
+      3000
+    );
+  }
+
+  dotClick() {
+    this.setState({ index: Number(event.target.name) });
+    clearInterval(this.timeID);
+    this.timeID = setInterval(
+      () => this.autoRun(),
+      3000
+    );
+
+  }
+
+  componentDidMount() {
+    this.timeID = setInterval(
+      () => this.autoRun(),
+      3000
+    );
+
   }
 
   render() {
+    const pics = this.props.pics;
+    const listPics = pics.map(pic =>
+    <img src={pic.url} key={pic.id} className={pic.id === this.state.index ? '' : 'hidden'} />
 
+    );
+
+    const listButtons = pics.map(pic =>
+      <button onClick={this.dotClick} className={pic.id === this.state.index ? 'dot dot-select' : 'dot'} name={pic.id} key={pic.id}></button>
+    );
     return (
       <div className="carousel-container">
         <div className=" arrow-holder column-one-fifth">
-          <i className="far fa-caret-square-left font-style"></i>
+          <i onClick={this.leftClick} className="far fa-caret-square-left font-style transition"></i>
         </div>
         <div className=" column-three-fifth pic-column">
           <div className="pic-holder">
-            <img src="https://cdn.mos.cms.futurecdn.net/G8tpf6HYLdXLxLHMKK3G-1200-80.jpg" />
+            {listPics}
           </div>
           <div className="dot-holder">
-            <button className="dot"></button>
-            <button className="dot"></button>
-            <button className="dot"></button>
-            <button className="dot"></button>
-            <button className="dot"></button>
+            {listButtons}
           </div>
         </div>
         <div className=" arrow-holder column-one-fifth">
-          <i className="far fa-caret-square-right font-style"></i>
+          <i onClick={this.rightClick} className="far fa-caret-square-right font-style transition"></i>
         </div>
       </div>
     );
--- react-carousel-component/src/index.jsx
+++ react-carousel-component/src/index.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Carousel from './carousel';
 
+const pics = [
+  {
+    id: 1,
+    url: 'https://cdn.mos.cms.futurecdn.net/G8tpf6HYLdXLxLHMKK3G-1200-80.jpg'
+  },
+
+  {
+    id: 2,
+    url: 'https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/08/final-fantasy-16-versus-13.jpg'
+  },
+
+  {
+    id: 3,
+    url: 'https://www.videogameschronicle.com/files/2020/10/Final-Fantasy-XVI-d-scaled.jpg'
+  }
+];
+
 ReactDOM.render(
-  <Carousel />,
+  <Carousel pics={pics} />,
   document.querySelector('#root')
 );
