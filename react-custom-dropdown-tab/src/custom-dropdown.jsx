807515ff74a2e2d81753396fcfa7e1ed609ee168
Kenneth Wang <yunfeiw@me.com>
2021-10-24 17:01:34 +0000

    in progress

--- react-custom-dropdown-tab/src/custom-dropdown.jsx
+++ react-custom-dropdown-tab/src/custom-dropdown.jsx
@@ -20,10 +20,12 @@ export default class CustomDropdown extends React.Component {
     return (
       <div className={!isOpen ? 'container' : 'container modal-on'} onClick={!isOpen ? null : this.switch}>
         <header className="logo-container">
+          <div onClick={this.switch} className="switch"><i className="fas fa-bars"></i></div>
           <h1 className="logo">ThriveVox</h1>
+          <div className="chat"><i className="far fa-comments"></i></div>
         </header>
-        <div onClick={this.switch} className="switch"><i className="fas fa-bars"></i></div>
         <div className={!isOpen ? 'menu' : ' menu menu-on'}>
+          <h1>Menu</h1>
           <a>About Me</a>
           <a>What I like</a>
           <a>Are You There</a>
