1622372d1e74eb037f238efc8afc9fe787a0ff12
Kenneth Wang <yunfeiw@me.com>
2021-10-25 20:15:58 +0000

    In progress

---
+++ react-custom-accordion/src/custom-accordion.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class CustomAccordion extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isOpen: '' };
+  }
+}
---
+++ react-custom-accordion/src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CustomAccordion from './custom-accordion';
+
+ReactDOM.render(
+  <CustomAccordion />,
+  document.querySelector('#root')
+);
