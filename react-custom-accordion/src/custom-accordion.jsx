15e8af7f579871c557a8d9b802084270c212c002
Kenneth Wang <yunfeiw@me.com>
2021-10-25 23:22:26 +0000

    Completed custom accrodion

--- react-custom-accordion/src/custom-accordion.jsx
+++ react-custom-accordion/src/custom-accordion.jsx
@@ -4,5 +4,32 @@ export default class CustomAccordion extends React.Component {
   constructor(props) {
     super(props);
     this.state = { isOpen: '' };
+    this.click = this.click.bind(this);
+  }
+
+  click() {
+    if (this.state.isOpen === event.target.getAttribute('data-id')) {
+      this.setState({ isOpen: '' });
+    } else {
+      this.setState({ isOpen: event.target.getAttribute('data-id') });
+    }
+  }
+
+  render() {
+    const sections = this.props.sections;
+    const listSections = sections.map(section =>
+      <div key={section.title}>
+        <div className="section-header" onClick={this.click} data-id={section.title}>{section.title}</div>
+        <div className={section.title === this.state.isOpen ? 'content-holder open' : 'content-holder close'}>
+          <div className={section.title === this.state.isOpen ? 'section-content' : 'section-content hidden'}>{section.content}</div>
+        </div>
+      </div>
+    );
+
+    return (
+        <div className="container">
+          {listSections}
+        </div>
+    );
   }
 }
