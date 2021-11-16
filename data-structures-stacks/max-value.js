fd9d3974c95f340bf6d7c0d1d6e6a4fa89f26f84
Kenneth Wang <yunfeiw@me.com>
2021-11-16 21:23:43 +0000

    modify code

--- data-structures-stacks/max-value.js
+++ data-structures-stacks/max-value.js
@@ -14,3 +14,11 @@ function maxValue(stack) {
   }
   return max;
 }
+
+// let max=-Infinity;
+
+// while(typeof stack.peek()!== 'undefined'){
+//   max=Math.max(max, stack.pop());
+
+// }
+// return max;
