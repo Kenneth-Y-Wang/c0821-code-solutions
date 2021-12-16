d1526fff203b278ed0df255a6b1d54554e60d0e3
Kenneth Wang <yunfeiw@me.com>
2021-12-16 02:31:40 +0000

    redo with arrow function and practice

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,28 +1,40 @@
-/* exported drop */
+// /* exported drop */
 
-// create a array storage for final outcome
-// if the given count is larger then the array length, then the empty array will be returned
-// if the given count is smaller then the array length, then we look at each element from array, from index count, to the last element
-// for each checked array element, push it to result
-// return result
+// // create a array storage for final outcome
+// // if the given count is larger then the array length, then the empty array will be returned
+// // if the given count is smaller then the array length, then we look at each element from array, from index count, to the last element
+// // for each checked array element, push it to result
+// // return result
 
-function drop(array, count) {
-  var result = [];
+// function drop(array, count) {
+//   var result = [];
+//   if (count > array.length) {
+//     result = [];
+//     return result;
+//   }
+//   for (var i = count; i < array.length; i++) {
+//     result.push(array[i]);
+//   }
+//   return result;
+// }
+
+// /* faster version with .slice without mutating the array
+
+// function drop(array, count) {
+//   var result = array.slice(count, array.length);
+//   return result;
+// }
+
+// */
+
+const drop = (array, count) => {
+  let result = [];
   if (count > array.length) {
     result = [];
-    return result;
-  }
-  for (var i = count; i < array.length; i++) {
-    result.push(array[i]);
+  } else {
+    for (let i = count; i < array.length; i++) {
+      result.push(array[i]);
+    }
   }
   return result;
-}
-
-/* faster version with .slice without mutating the array
-
-function drop(array, count) {
-  var result = array.slice(count, array.length);
-  return result;
-}
-
-*/
+};
