571de03455054bed9e2a3d58989f265376eadfe1
Kenneth Wang <yunfeiw@me.com>
2021-12-17 09:08:31 +0000

    redo with arrow function and practice

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,15 +1,23 @@
-/* exported invert */
+// /* exported invert */
 
-// create a result empty object for final result
-// check each property from source object, one at a time
-// since the goal for the function is to swap the property with value, so we add the property value as new property, and the orginal property as new value, to the final object
-// return the final object as the final result
+// // create a result empty object for final result
+// // check each property from source object, one at a time
+// // since the goal for the function is to swap the property with value, so we add the property value as new property, and the orginal property as new value, to the final object
+// // return the final object as the final result
 
-function invert(source) {
-  var result = {};
-  for (var key in source) {
+// function invert(source) {
+//   var result = {};
+//   for (var key in source) {
+//     result[source[key]] = key;
+
+//   }
+//   return result;
+// }
+
+const invert = source => {
+  const result = {};
+  for (const key in source) {
     result[source[key]] = key;
-
   }
   return result;
-}
+};
