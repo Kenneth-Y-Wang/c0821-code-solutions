bb49081684940b0b0ce4ac57e09326f97d4ebc3e
Kenneth Wang <yunfeiw@me.com>
2021-12-28 02:08:35 +0000

    redo with arrow function and es6

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -2,24 +2,47 @@ var $tabContainer = document.querySelector('.tab-container');
 var $tab = document.querySelectorAll('.tab');
 var $view = document.querySelectorAll('.view');
 
-$tabContainer.addEventListener('click', function (e) {
+// $tabContainer.addEventListener('click', function (e) {
+//   if (event.target.matches('.tab') === false) {
+//     return;
+//   }
+//   for (var i = 0; i < $tab.length; i++) {
+//     if ($tab[i] === event.target) {
+//       $tab[i].className = 'tab active';
+//     } else {
+//       $tab[i].className = 'tab';
+//     }
+//   }
+
+//   var dataView = event.target.getAttribute('data-view');
+//   for (var j = 0; j < $view.length; j++) {
+//     if ($view[j].getAttribute('data-view') === dataView) {
+//       $view[j].className = ' view ';
+//     } else {
+//       $view[j].className = ' view hidden';
+//     }
+//   }
+// });
+
+$tabContainer.addEventListener('click', event => {
   if (event.target.matches('.tab') === false) {
     return;
   }
-  for (var i = 0; i < $tab.length; i++) {
+
+  for (let i = 0; i < $tab.length; i++) {
     if ($tab[i] === event.target) {
       $tab[i].className = 'tab active';
+
     } else {
       $tab[i].className = 'tab';
     }
   }
 
-  var dataView = event.target.getAttribute('data-view');
-  for (var j = 0; j < $view.length; j++) {
-    if ($view[j].getAttribute('data-view') === dataView) {
-      $view[j].className = ' view ';
+  for (let i = 0; i < $view.length; i++) {
+    if ($view[i].getAttribute('data-view') === event.target.getAttribute('data-view')) {
+      $view[i].className = 'view';
     } else {
-      $view[j].className = ' view hidden';
+      $view[i].className = 'view hidden';
     }
   }
 });
